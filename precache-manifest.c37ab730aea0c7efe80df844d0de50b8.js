47ad06a115849a19232f0468d6d1d19c88a03087
Bharath Kumar <pbkbharath1707@gmail.com>
2020-09-24 13:03:01 +0530

    Updates

---
+++ precache-manifest.c37ab730aea0c7efe80df844d0de50b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04acee80c86a59635f8007ec92043637",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "d92323b96c83894f02d2",
+    "url": "/Covid19-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b2ba060bbcc95523bf4a",
+    "url": "/Covid19-Tracker/static/css/main.52a43d12.chunk.css"
+  },
+  {
+    "revision": "d92323b96c83894f02d2",
+    "url": "/Covid19-Tracker/static/js/2.574507ad.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid19-Tracker/static/js/2.574507ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ba060bbcc95523bf4a",
+    "url": "/Covid19-Tracker/static/js/main.3e3ce84d.chunk.js"
+  },
+  {
+    "revision": "d3314dffd8e2ce5a639d",
+    "url": "/Covid19-Tracker/static/js/runtime-main.a371065b.js"
+  }
+]);
